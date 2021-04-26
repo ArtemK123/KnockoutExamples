ccb49f8118366cd0086517ecfea69237f0124db6
ArtemK123 <darkmode1012@gmail.com>
2021-04-26 09:43:03 +0300

    Added simple nodejs server for serving static files

---
+++ server.js
@@ -0,0 +1,57 @@
+const  http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 12211;
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    const extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                response.writeHead(404);
+                response.end();
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(port);
+console.log(`Server running at http://127.0.0.1:${port}/`);
