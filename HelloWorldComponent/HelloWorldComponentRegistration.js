0622b57f30741340797dcdaf73830a912935fecc
ArtemK123 <darkmode1012@gmail.com>
2021-04-26 09:44:06 +0300

    Extracted separate component and used it in index.html

--- HelloWorldComponent/HelloWorldComponentRegistration.js
+++ HelloWorldComponent/HelloWorldComponentRegistration.js
@@ -1,20 +1,6 @@
-const ViewModel = function() {
-  this.greeting = ko.observable("Hello");
-  this.name = ko.observable("World");
-  this.appHeading = ko.pureComputed(function() {
-    return this.greeting() + ", " + this.name() + "!";
-  }, this);
-}
-
-const templateHtml = `
-<section>
-  <h1 data-bind="text: appHeading"></h1>
-  <p>Greeting: <input data-bind="textInput: greeting" /></p>
-  <p>Name: <input data-bind="textInput: name" /></p>
-</section>
-`;
-
-ko.components.register("hello-world", {
-  viewModel: ViewModel,
-  template: templateHtml
-});
+define(["scripts/knockout.js"], (ko) => {
+  ko.components.register("hello-world", {
+    viewModel: { require: "HelloWorldComponent/HelloWorldViewModel.js" },
+    template: { require: "text!HelloWorldComponent/HelloWorld.html" }
+  });
+})
--- HelloWorldComponent/HelloWorldViewModel.js
+++ HelloWorldComponent/HelloWorldViewModel.js
@@ -1,9 +1,9 @@
-const ViewModel = function() {
-  this.greeting = ko.observable("Hello");
-  this.name = ko.observable("World");
-  this.appHeading = ko.pureComputed(function() {
-    return this.greeting() + ", " + this.name() + "!";
-  }, this);
-}
-
-export default ViewModel;
+define(["scripts/knockout.js"], function (ko) {
+  return function() {
+    this.greeting = ko.observable("Hello");
+    this.name = ko.observable("World");
+    this.appHeading = ko.pureComputed(function() {
+      return this.greeting() + ", " + this.name() + "!";
+    }, this);
+  }
+});
