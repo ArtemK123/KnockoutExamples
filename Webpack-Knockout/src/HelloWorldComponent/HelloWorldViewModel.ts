125b108da8b596eb41e07271b32cc274f1a71589
ArtemK123 <darkmode1012@gmail.com>
2021-04-29 08:55:41 +0300

    Transfered HelloWorld component to typescript

---
+++ Webpack-Knockout/src/HelloWorldComponent/HelloWorldViewModel.ts
@@ -0,0 +1,16 @@
+import html from "./HelloWorld.html";
+import * as ko from "knockout";
+
+class HelloWorldViewModel {
+  public greeting: ko.Observable<string>;
+  public name: ko.Observable<string>;
+  public appHeading: ko.PureComputed<string>;
+
+  constructor() {
+    this.greeting = ko.observable<string>("Hello");
+    this.name = ko.observable<string>("World");
+    this.appHeading = ko.pureComputed<string>(() => this.greeting() + ", " + this.name() + "!");
+  }
+}
+
+export default { viewModel: HelloWorldViewModel, template: html };
